ba79c9e1c8221760ade153c4c64dcbdc1be7df6f
Male Michael <malmike21@gmail.com>
2025-07-15 13:58:18 +0200

    daad scraper crewai custom tool

---
+++ research_daad/src/research_daad/tools/daad_scraper_handler.py
@@ -0,0 +1,13 @@
+from research_daad.src.research_daad.tools.daad_scraper import DAADScholarshipScraper
+from crewai.tools import tool
+
+
+@tool("scrape_daad_scholarships")
+def scrape_daad_scholarships() -> str:
+    """Scrapes all active scholarships from the DAAD database"""
+
+    scraper = DAADScholarshipScraper(headless=True)
+    all_scholarships = scraper.scrape_scholarships()
+    if not all_scholarships:
+        return "No scholarships found."
+    return str(all_scholarships)
